e6958752c3988877d4402adf598cc0690e2fa023
Fernando Dias <ferpaesdias@gmail.com>
2025-03-27 11:24:32 -0300

    Mostrar DNS no Output

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "Dados_VMS" {
   value = format(
-    "\nTamanho das VMs = %s \nPreço por hora (dólar) = US$ %s ",
+    "\tTamanho de cada VM = %s \n\tPreço por hora de cada VM = US$ %s ",
       trimspace(replace(base64decode(data.external.vmspot.result.size), "\"", "")), 
       trimspace(replace(base64decode(data.external.vmspot.result.price), "\"", ""))
   ) 
@@ -11,10 +11,27 @@ output "Acesso_SSH" {
     "\n",
     [for i in azurerm_linux_virtual_machine.vm :
       format(
-        "VM = %s | ssh -o StrictHostKeyChecking=no -l adminuser -p 22 -i id_rsa %s",
+        "\tVM = %s | ssh -o StrictHostKeyChecking=no -l adminuser -p 22 -i id_rsa %s",
         i.name, 
         i.public_ip_address,
       )
     ]
   )
 }
+
+output "DNS" {
+  value = join(
+    "\n",
+    [for i in azurerm_public_ip.public_ip :
+      format(
+        "\t%s",
+        i.fqdn
+      )
+    ]
+  )
+}
+
+output "kubeconfig" {
+  value = "\tPara acessar o cluster do kubectl de sua máquina execute o comando abaixo:\n\texport KUBECONFIG=$PWD/kubeconfig"
+}
+
